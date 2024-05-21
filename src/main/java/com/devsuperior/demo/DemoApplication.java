341f87a402ec16845c1969b03f5b64c51933c441
pativilaka <p.vilaka@gmail.com>
2024-05-21 14:41:29 -0300

    add bcrypt password

--- src/main/java/com/devsuperior/demo/DemoApplication.java
+++ src/main/java/com/devsuperior/demo/DemoApplication.java
@@ -1,13 +1,25 @@
 package com.devsuperior.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication  {
+
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+//	@Override
+//	public void run(String... args) throws Exception {
+//		System.out.println("Encode = " + passwordEncoder.encode("123456"));
+//		System.out.println("Comparação = " + passwordEncoder.matches("123456",
+//				"$2a$10$Sw8dPl74Galwf/vxyuIWPetrKoNfdjj.Xo6Y2WJpnCwbQhy7583Ne"));
+//	}
+	
 }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$Sw8dPl74Galwf/vxyuIWPetrKoNfdjj.Xo6Y2WJpnCwbQhy7583Ne');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$Sw8dPl74Galwf/vxyuIWPetrKoNfdjj.Xo6Y2WJpnCwbQhy7583Ne');
 
 INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
